3a3bfd9f9ce0accbe214deea7926010f4ea18ec0
altimate <florian.mart@gmail.com>
2017-02-06 14:33:52 +0100

    add gestion user

---
+++ src/UserBundle/Resources/public/plugins/summernote/summernote-example.js
@@ -0,0 +1,28 @@
+// Summernotes
+// ===========
+$(document).ready(function() {
+if($('.summernote-fixed-height').length) {
+	$('.summernote-fixed-height').summernote({
+		height: 300,
+		dialogsInBody: true,
+		dialogsFade: true
+	});
+}
+
+if($('.summernote-auto').length) {
+	$('.summernote-auto').summernote({
+		dialogsInBody: true,
+		dialogsFade: true
+	});
+}
+
+if($('.summernote-airmode').length) {
+	$('.summernote-airmode').summernote({
+		height: 300,
+		tabsize: 2,
+		airMode: true,
+		dialogsInBody: true,
+		dialogsFade: true
+	});
+}
+})
