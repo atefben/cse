0fdc65180cc46fc6aa7c0766a47f06353fe35ae0
altimate <florian.mart@gmail.com>
2017-02-07 16:58:48 +0100

    finalisation add and edit user

--- src/UserBundle/Controller/UserController.php
+++ src/UserBundle/Controller/UserController.php
@@ -3,8 +3,9 @@
 namespace UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
-
+use Symfony\Component\HttpFoundation\Request;
+use UserBundle\Entity\User;
+use UserBundle\Form\UserType;
 
 class UserController extends Controller
 {
@@ -15,5 +16,51 @@ class UserController extends Controller
         $repository = $this->getDoctrine()->getRepository('UserBundle:User');
         $lists =  $repository->findAll();
     	return $this->render('UserBundle::index.html.twig', array('lists'=>$lists));
+    }
+
+
+    public function AddAction(Request $request) {
+
+        $User = new User();
+        $form = $this->createForm(UserType::class, $User);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($User);
+            $em->flush($User);
+
+            return $this->redirectToRoute('user_edit', array('id' => $User->getId()));
+        }
+
+
+        return $this->render('UserBundle::add.html.twig', array('form'=>$form->createView()));
+    }
+
+    public function EditAction(Request $request) {
+        //$User = new User();
+        $id = $request->get('id');
+
+        $repository = $this->getDoctrine()->getRepository('UserBundle:User');
+        $User =  $repository->find($id);
+
+        $form = $this->createForm(UserType::class, $User);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($User);
+            $em->flush($User);
+
+            return $this->redirectToRoute('user_edit', array('id' => $User->getId()));
+        }
+
+
+        return $this->render('UserBundle::edit.html.twig', array('form'=>$form->createView()));
+    }
+
+
+    public function DeleteAction(Request $request) {
+
     }
 }
---
+++ src/UserBundle/Form/UserType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+use FOS\UserBundle\Util\LegacyFormHelper;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+
+class UserType  extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname')
+            ->add('lastname')
+            ->add('email')
+            ->add('username')
+            ->add('plainPassword')
+            ->add('enabled', HiddenType::class, array('data' => true ))
+
+           ->add('roles',  CollectionType::class, array(
+               'entry_type'   => ChoiceType::class,
+               'entry_options'  => array(
+                   'label' => false,
+                   'choices'  => array(
+                       'Administrateur' => 'ROLE_ADMIN',
+                       'Responsable'     => 'ROLE_RESPONSABLE_AGENCE',
+                       'Commercial'    => 'ROLE_COMMERCIAL',
+                   ),
+               ),
+           ))
+        ;
+    }
+}
